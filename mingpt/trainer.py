1fbbed95b986f8bbf14473f6fe50fd4321fa388e
Gav Gray <gngdb.labs@gmail.com>
2022-09-06 05:05:29 -0400

    using grad accumulation steps

--- mingpt/trainer.py
+++ mingpt/trainer.py
@@ -4,6 +4,7 @@ so nothing in this file really has anything to do with GPT specifically.
 """
 
 import time
+import pickle
 from collections import defaultdict
 
 import torch
@@ -25,13 +26,13 @@ class Trainer:
         C.learning_rate = 3e-4
         C.betas = (0.9, 0.95)
         C.weight_decay = 0.1 # only applied on matmul weights
+        C.accumulate_grad_batches = 1
         C.grad_norm_clip = 1.0
         return C
 
-    def __init__(self, config, model, train_dataset):
+    def __init__(self, config, model, train_dataset, **extra_state):
         self.config = config
         self.model = model
-        self.optimizer = None
         self.train_dataset = train_dataset
         self.callbacks = defaultdict(list)
 
@@ -47,6 +48,35 @@ class Trainer:
         self.iter_num = 0
         self.iter_time = 0.0
         self.iter_dt = 0.0
+        for k,v in extra_state.items():
+            setattr(self, k, v)
+
+    def load(self, save_path):
+        # set attributes that were saved
+        with open(save_path, 'rb') as f:
+            save_dict = pickle.load(f)
+        self.optimizer_state_dict = save_dict['optimizer']
+        del save_dict['optimizer']
+        for k,v in save_dict.items():
+            setattr(self, k, v)
+        return self
+
+    def save(self, save_path):
+        # parse out objects that can be pickled and pickle them
+        def is_pickleable(obj):
+            # quack quack quack
+            try:
+                pickle.dumps(obj)
+                return True
+            except AttributeError:
+                return False
+        save_dict = {k:v for k,v in self.__dict__.items() if is_pickleable(v)}
+        del save_dict['train_dataset'] # this is a huge storage waste
+        del save_dict['callbacks'] # these are dynamic and can't be pickled
+        if 'optimizer' in save_dict:
+            save_dict['optimizer'] = self.optimizer.state_dict()
+        with open(save_path, 'wb') as f:
+            pickle.dump(save_dict, f)
 
     def add_callback(self, onevent: str, callback):
         self.callbacks[onevent].append(callback)
@@ -63,6 +93,8 @@ class Trainer:
 
         # setup the optimizer
         self.optimizer = model.configure_optimizers(config)
+        if hasattr(self, 'optimizer_state_dict'):
+            self.optimizer.load_state_dict(self.optimizer_state_dict)
 
         # setup the dataloader
         train_loader = DataLoader(
@@ -70,31 +102,36 @@ class Trainer:
             sampler=torch.utils.data.RandomSampler(self.train_dataset, replacement=True, num_samples=int(1e10)),
             shuffle=False,
             pin_memory=True,
-            batch_size=config.batch_size,
+            batch_size=config.batch_size//config.accumulate_grad_batches,
             num_workers=config.num_workers,
         )
 
         model.train()
-        self.iter_num = 0
         self.iter_time = time.time()
         data_iter = iter(train_loader)
         while True:
-
-            # fetch the next batch (x, y) and re-init iterator if needed
-            try:
-                batch = next(data_iter)
-            except StopIteration:
-                data_iter = iter(train_loader)
-                batch = next(data_iter)
-            batch = [t.to(self.device) for t in batch]
-            x, y = batch
-
-            # forward the model
-            logits, self.loss = model(x, y)
-
-            # backprop and update the parameters
+            self.grad_accumulate_batch = 0
+            self.loss = 0
             model.zero_grad(set_to_none=True)
-            self.loss.backward()
+            while self.grad_accumulate_batch < config.accumulate_grad_batches:
+                # fetch the next batch (x, y) and re-init iterator if needed
+                try:
+                    batch = next(data_iter)
+                except StopIteration:
+                    data_iter = iter(train_loader)
+                    batch = next(data_iter)
+                batch = [t.to(self.device) for t in batch]
+                x, y = batch
+
+                # forward the model
+                logits, loss = model(x, y)
+
+                # backprop
+                loss = loss/config.accumulate_grad_batches
+                loss.backward()
+                self.loss += loss.detach() # this would cause a memory leak if we didn't detach
+                self.grad_accumulate_batch += 1
+            # and update the parameters
             torch.nn.utils.clip_grad_norm_(model.parameters(), config.grad_norm_clip)
             self.optimizer.step()
 
